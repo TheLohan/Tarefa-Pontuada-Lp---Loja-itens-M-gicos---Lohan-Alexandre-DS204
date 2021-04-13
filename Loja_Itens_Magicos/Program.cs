80b88666a6e8dd733a32506c1c22ff47ba40ae86
Lohan Alexandre <51961163+TheLohan@users.noreply.github.com>
2021-04-12 22:02:46 -0300

    Add files via upload

---
+++ Loja_Itens_Magicos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Loja_Itens_Magicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int alternativa = 0;
+            Loja loja = new Loja();
+
+            do
+            {
+                Console.WriteLine("1 - Sou o Mercante");
+                Console.WriteLine("2 - Sou cliente");
+                Console.WriteLine("0 - encerrar \n");
+                alternativa = Convert.ToInt16(Console.ReadLine());
+
+                if (alternativa == 1)
+                {
+                    loja.AddCatalogo();
+
+                }
+                else if (alternativa == 2)
+                {
+                    Personagem aventureiro = new Personagem();
+                    aventureiro.ClienteEscolha(loja, aventureiro);
+                }
+                else if (alternativa != 0)
+                {
+                    Console.WriteLine("Entrada Invalida");
+                }
+            } while (alternativa != 0);
+            
+        }
+
+        
+
+    }
+}
